2b0d4af92a95cbb4fe5e03cd78ecb6f066e9bbab
ThatBenKraft <ben.kraft@rcn.com>
2023-04-21 13:50:30 -0400

    Modulization

---
+++ __init__.py
@@ -0,0 +1,13 @@
+"""
+# Create 3 ROS Interface
+This library is for the multi-purpose of the iRobot Create®3. Other 
+hardware/software support for Raspberry Pi 3 Camera, Airtable API 
+controllers, as well as Teachable Machines machine learning model controllers.
+
+## Publishers
+Contains multiple publisher preset nodes, including motor controllers and midi 
+audio drivers.
+
+## Subscribers
+Contains IR sensor subsriber nodes.
+"""
